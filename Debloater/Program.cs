2beee8665625cf7da105c086f16b0a8dc1fa1ae1
Travis Owens <travisjamesowens@gmail.com>
2020-12-21 09:30:48 -0500

    Adding If Statements

--- Debloater/Program.cs
+++ Debloater/Program.cs
@@ -7,9 +7,28 @@ namespace Debloater
     {
         static void Main(string[] args)
         {
+            //Console Appearance
+            Console.Title = "Debloater Console";
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            // Warning String
+            string warn = "WARNING This may destroy your windows install. Please do not use this if your a beginner with Powershell or VM's" ;
             Console.WriteLine("Welcome to the Windows Debloater Console!");
             Console.WriteLine("This will debloat Windows 10 on your PC or VM");
-            Console.WriteLine("");
+            Console.WriteLine("This can Destroy your install if you are not careful");
+            // Confirmation
+            Console.WriteLine(warn);
+            Console.Write("Are You Sure You want to do this Destructive Action THIS CANNOT BE UNDONE. Type true or false to confirm or deny:");
+            bool yorn = Convert.ToBoolean (Console.ReadLine());
+
+            if (yorn == true);
+            {
+
+                Process.Start(@"powershell.exe", @"/c " + "");
+
+            }
+
 
         }
     }
