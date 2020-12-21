15d5d266c7a2568ceb26e661c3f4b39dec95a346
Travis Owens <travisjamesowens@gmail.com>
2020-12-21 08:52:00 -0500

    Add project files.

---
+++ Debloater/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Debloater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Windows Debloater Console!");
+            Console.WriteLine("This will debloat Windows 10 on your PC or VM");
+            Console.WriteLine("");
+
+        }
+    }
+}
